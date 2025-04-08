2ba63a8f60e548bdbd3e021c5e07b83cf47415d5
Yahir Jasso <jasoyahir2003@gmail.com>
2025-04-08 13:19:22 -0500

    Updated the syntax for the xacro and launch file for ros2 humble

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,8 +3,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 import xacro
@@ -12,30 +12,34 @@ import xacro
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
+    # Launch configuration variables
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('asmr_ros'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    # Get the path to the xacro file
+    pkg_path = get_package_share_directory('asmr_ros')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+
+    # Process the xacro file
     robot_description_config = xacro.process_file(xacro_file)
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    robot_description = robot_description_config.toxml()
+
+    # Robot State Publisher Node
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        name='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': use_sim_time
+        }]
     )
 
-
-    # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
-            'use_sim_time',
+            name='use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
+            description='Use simulation (Gazebo) clock if true'
+        ),
         node_robot_state_publisher
     ])
